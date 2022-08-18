eec7faff8688596d8a8f07df30ac42e02abbc943
Nifer Kilakila <nifercritter@gmail.com>
2022-08-18 09:21:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Creates a super simple Express app which shows the basic way to register a Handlebars view engine using this package.
+
+// import express from 'express';
+// import { engine } from 'express-handlebars';
+
+// const app = express();
+
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+// app.listen(3000);
