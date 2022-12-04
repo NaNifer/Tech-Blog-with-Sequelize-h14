d4e141573918e160836e9eaf03fc5da1d557ad00
Nifer Kilakila <nifercritter@gmail.com>
2022-12-03 16:15:15 -0800

    Dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,5 +3,15 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+//Get All existing Posts associated to a specific User
+
+router.get("/", withAuth, async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
 
 module.exports = router;
