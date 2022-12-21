37c0059f6cd0016c92312ba87de038c63bbcc78c
Nifer Kilakila <nifercritter@gmail.com>
2022-12-20 18:00:34 -0800

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+
+
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
@@ -7,9 +9,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    `${process.env.DB_NAME}`,
-    `${process.env.DB_USER}`,
-    `${process.env.DB_PASSWORD}`,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
